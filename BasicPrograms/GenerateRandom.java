351319494795d6170d33a6c56efb96257d7bbf65
prasanna <94883909+prasanna1225@users.noreply.github.com>
2024-05-21 11:46:25 +0530

    Create GenerateRandom.java

---
+++ BasicPrograms/GenerateRandom.java
@@ -0,0 +1,10 @@
+import java.lang.Math;   
+public class GenerateRandom
+{   
+public static void main(String args[])   
+{   
+
+System.out.println("1st Random Number: " + Math.random());   
+System.out.println("2nd Random Number: " + Math.random());   
+}   
+}  
