33ec0f90efe93872be271d3f8a79fed21b0f2b5e
prasanna <94883909+prasanna1225@users.noreply.github.com>
2024-05-22 10:40:58 +0530

    Update ReverseNumber.java

--- Number Programs/ReverseNumber.java
+++ Number Programs/ReverseNumber.java
@@ -1,1 +1,23 @@
+package reversenumber;
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int res=0;
+		while(n>0) {
+			int rem=n%10;
+			res=res*10+rem;
+			n=n/10;
+			
+		}
+		
+		System.out.println(res);
+
+	}
+
+}
 
