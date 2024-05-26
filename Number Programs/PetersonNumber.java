3823ac4851173364e36abb9485ffef2f20105477
prasanna <94883909+prasanna1225@users.noreply.github.com>
2024-05-26 21:52:02 +0530

    Update PetersonNumber.java

--- Number Programs/PetersonNumber.java
+++ Number Programs/PetersonNumber.java
@@ -1,1 +1,39 @@
+package petersonnumber;
+
+import java.util.Scanner;
+
+public class PetersonNumber {
+
+    // Method to calculate factorial of a number
+    public static int factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();  
+        int originalNum = num; // Store original number for comparison
+        int sum = 0;
+
+        while (num > 0) {
+            int rem = num % 10; // Get the last digit
+            sum += factorial(rem); // Add factorial of the digit to sum
+            num /= 10; // Remove the last digit
+        }
+
+        // Check if sum of factorials equals the original number
+        if (sum == originalNum) {
+            System.out.println("Peterson Number");
+        } else {
+            System.out.println("Not Peterson Number");
+        }
+
+        sc.close(); // Close the scanner
+    }
+}
+
 
