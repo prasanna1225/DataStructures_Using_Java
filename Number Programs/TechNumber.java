c4c9cdc32d66f395d2e535fb2557a22bbcb9a184
prasanna <94883909+prasanna1225@users.noreply.github.com>
2024-05-30 10:44:42 +0530

    Update TechNumber.java

--- Number Programs/TechNumber.java
+++ Number Programs/TechNumber.java
@@ -1,1 +1,33 @@
+package technumber;
+import java.util.Scanner;
+public class TechNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		int n= sc.nextInt();
+		int digits=0;
+		int firstHalf, secondHalf,squareNum=0;
+		int num=n;
+		while(num>0) {
+			digits++;
+			num/=10;
+		}
+		if(digits%2==0) {
+			num=n;
+			firstHalf=num%(int)Math.pow(10,digits/2);
+			secondHalf=num/(int)Math.pow(10,digits/2);
+			squareNum=(firstHalf+secondHalf)*(firstHalf+secondHalf);
+			if(squareNum==n) {
+				System.out.println("tech number");
+			}else {
+				System.out.println("It can't be a tech number");
+			}
+		}
+		else {
+			System.out.println("It can't be a tech number");
+		}
+	}
+
+}
 
