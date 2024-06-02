970f2096fdb134eb8c953d6ac43379064764131b
prasanna <94883909+prasanna1225@users.noreply.github.com>
2024-06-02 09:50:01 +0530

    Update SumOfNaturalNum.java

--- BasicPrograms/SumOfNaturalNum.java
+++ BasicPrograms/SumOfNaturalNum.java
@@ -1,1 +1,19 @@
+package sumofnaturalnum;
+import java.util.Scanner;
+
+public class SumOfNaturalNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		int sum=0;
+		for(int i=0;i<=num;i++) {
+			sum+=i;
+		}
+		System.out.println(sum);
+
+	}
+
+}
 
