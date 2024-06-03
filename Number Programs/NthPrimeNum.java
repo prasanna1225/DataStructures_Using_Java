79e60bb60936f3dc8251f6840dc636a4e389928d
prasanna <94883909+prasanna1225@users.noreply.github.com>
2024-06-03 10:45:20 +0530

    Update NthPrimeNum.java

--- Number Programs/NthPrimeNum.java
+++ Number Programs/NthPrimeNum.java
@@ -1,1 +1,26 @@
-
+import java.util.Scanner;  
+public class NthPrimeNumber   
+{  
+public static void main(String[] args)   
+{   
+Scanner sc = new Scanner(System.in);  
+int n = sc.nextInt();   
+int num=1, count=0, i;  
+while (count < n)  
+{  
+num=num+1;  
+for (i = 2; i <= num; i++)  
+{   
+if (num % i == 0)   
+{  
+break;  
+}  
+}  
+if (i == num)  
+{  
+count = count+1;  
+}  
+}  
+System.out.println("The " +n +"th prime number is: " + num);  
+}  
+}  
