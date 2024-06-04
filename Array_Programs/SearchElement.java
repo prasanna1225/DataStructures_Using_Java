8efa4e9dc5376a3c2579e02b1bdd382a82e0b71a
prasanna <94883909+prasanna1225@users.noreply.github.com>
2024-06-04 21:37:26 +0530

    Update SearchElement.java

--- Array_Programs/SearchElement.java
+++ Array_Programs/SearchElement.java
@@ -1,1 +1,30 @@
+package searchelement;
+import java.util.Scanner;
+
+public class SearchElement {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int numFind=sc.nextInt();
+		
+		int arr[]=new int[n];
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+		// TODO Auto-generated method stub
+		for(int j=0;j<arr.length;j++) {
+		    if(arr[j]==numFind) {
+		    	System.out.println("Element Found");
+		    }
+		    
+		}
+	
+		
+		
+
+	}
+	
+
+}
 
