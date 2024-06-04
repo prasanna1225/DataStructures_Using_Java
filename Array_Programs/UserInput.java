c9f2119ceb53631aca803c8ef63b4f0e1f0f91de
prasanna <94883909+prasanna1225@users.noreply.github.com>
2024-06-04 14:09:01 +0530

    Update UserInput.java

--- Array_Programs/UserInput.java
+++ Array_Programs/UserInput.java
@@ -1,1 +1,20 @@
+package userinput;
+import java.util.Scanner;
+
+public class UserInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner sc= new Scanner(System.in);
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+    for(int i=0;i<arr.length;i++) {
+    arr[i]=sc.nextInt();    
+    }
+    for(int i=0;i<arr.length;i++) {
+    	System.out.print(arr[i]+" ");
+    }
+	}
+
+}
 
