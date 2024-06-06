25c770ea4cf7c8fe1a7a4ce8fa778a61c010aba5
prasanna <94883909+prasanna1225@users.noreply.github.com>
2024-06-06 11:02:33 +0530

    Update RightRotatingArray.java

--- Array_Programs/RightRotatingArray.java
+++ Array_Programs/RightRotatingArray.java
@@ -1,1 +1,27 @@
+package rightrotatingarray;
+import java.util.Scanner;
+
+public class RightRotatingArray {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+		// TODO Auto-generated method stub
+		int temp=arr[arr.length-1];
+		for(int i=arr.length-1;i>0;i--) {
+			arr[i]=arr[i-1];
+		}
+		arr[0]=temp;
+		
+		for(int i=0;i<arr.length;i++) {
+		System.out.println(arr[i]);
+		}
+
+	}
+
+}
 
