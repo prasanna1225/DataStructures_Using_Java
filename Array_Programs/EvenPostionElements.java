a059311f134e6179f5b5d35b681c3e173b2d8c1d
prasanna <94883909+prasanna1225@users.noreply.github.com>
2024-06-07 11:26:04 +0530

    Create EvenPostionElements.java

---
+++ Array_Programs/EvenPostionElements.java
@@ -0,0 +1,27 @@
+package evenpostionelements;
+import java.util.Scanner;
+
+public class EvenPostionElements {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];	
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+		
+		
+		for(int i=1;i<arr.length;i=i+2) {
+			
+				System.out.println(arr[i]+" ");
+		
+		}
+		
+		
+		}
+		
+
+	}
+
+
