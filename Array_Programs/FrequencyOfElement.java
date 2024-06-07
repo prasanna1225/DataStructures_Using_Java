b4a092909e41f560b05957062f4b56db9489568c
prasanna <94883909+prasanna1225@users.noreply.github.com>
2024-06-07 11:07:08 +0530

    Update FrequencyOfElement.java

--- Array_Programs/FrequencyOfElement.java
+++ Array_Programs/FrequencyOfElement.java
@@ -1,7 +1,7 @@
-package rotatingarray;
+package frequencyofelement;
 import java.util.Scanner;
 
-public class RotatingArray {
+public class FrequencyOfElement {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
