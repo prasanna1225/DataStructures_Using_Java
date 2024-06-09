be681e3f37762bd1c8757a8c188e23007f73f018
prasanna <94883909+prasanna1225@users.noreply.github.com>
2024-06-09 10:27:23 +0530

    Update ArrayDescendingOrder.java

--- Array_Programs/ArrayDescendingOrder.java
+++ Array_Programs/ArrayDescendingOrder.java
@@ -1,1 +1,34 @@
+package rotatingarray;
+import java.util.Scanner;
+
+public class RotatingArray {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];	
+		int temp=0;
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+		
+	   for(int i=0;i<arr.length;i++) {
+		   for(int j=i+1;j<arr.length;j++) {
+			   if(arr[i]<arr[j]) {
+				   temp=arr[i];
+				   arr[i]=arr[j];  
+				   arr[j]=temp;
+		       }
+		   }
+		}
+	   for(int i=0;i<arr.length;i++) {
+		   System.out.print(arr[i]+" ");
+	   }
+	}
+}
+		
+
+	
+
+
 
