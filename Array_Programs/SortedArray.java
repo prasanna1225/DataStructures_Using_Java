bf91358614183fead00a384e6b9f22a5e7b5062f
prasanna <94883909+prasanna1225@users.noreply.github.com>
2024-06-10 09:29:05 +0530

    Update SortedArray.java

--- Array_Programs/SortedArray.java
+++ Array_Programs/SortedArray.java
@@ -1,1 +1,27 @@
+package rotatingarray;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RotatingArray {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];	
+		int temp=0;
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+		
+	   Arrays.sort(arr);
+	   for(int i=0;i<arr.length;i++) {
+	   System.out.println(arr[i]);
+	   }
+	}
+}
+		
+
+	
+
+
 
