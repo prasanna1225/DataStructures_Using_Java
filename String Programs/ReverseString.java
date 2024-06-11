a730b469b36c5930cfbf92cdaf5b7e2f759a7d29
prasanna <94883909+prasanna1225@users.noreply.github.com>
2024-06-11 10:03:10 +0530

    Update ReverseString.java

--- String Programs/ReverseString.java
+++ String Programs/ReverseString.java
@@ -1,1 +1,22 @@
+package reversestring;
+import java.util.Scanner;
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String s;  
+		Scanner sc=new Scanner(System.in);  
+		
+		s=sc.nextLine();                    
+		
+		for(int i=s.length();i>0;--i)               
+		{  
+		System.out.print(s.charAt(i-1));            
+		}  
+	}
+}
+		
+
+	
+
+
 
